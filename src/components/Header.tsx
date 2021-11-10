28133127e248c9543fc3c00f518c778cc2ec6cad
ddfilipov <denisfilipov12+3@gmail.com>
2021-11-10 23:00:17 +0100

    created a links for the router

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+    return (
+        <>
+            <h1>React Router</h1>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/profile">Profile</a>
+                </li>
+            </ul>
+        </>
+    );
+};
+
+export default Header;
